3687edf8a8b1bbcf482bae5a19e32807f6daf0c0
Adam-Barto <adam.jbarto@gmail.com>
2023-04-11 11:00:34 -0400

    Removed Token

--- make_requests.py
+++ make_requests.py
@@ -2,11 +2,14 @@ from urllib import request
 import json
 
 
-token = 'ByIYbVDPnRpUdoaCtYcUtRAarxTZpOCS' # Yeah this is a bad idea to post this sorta stuff on Github.
+key_file = open('token', 'r')
+token = key_file.read() # Let's grab that Key here, and NOT upload it to Github.
+key_file.close()
+
 file_save_location = 'outputfiles' # This is made to store the files
 num = 0
 offset = 1
-limit = 1 # should have kept this small
+limit = 1_000  # should have kept this small
 amount = 39
 
 for i in range(amount):
@@ -14,8 +17,8 @@ for i in range(amount):
     the_request = request.Request(url_string, headers={'token': token})
     with request.urlopen(the_request) as f:
         the_json = json.loads(f.read())
-        with open(file_save_location + f'/locations_{num}.json', 'w') as f:
-            json.dump(the_json, f)
+        with open(file_save_location + f'/locations_{num}.json', 'w') as f1:
+            json.dump(the_json, f1)
         num += 1
-        offset += 1000
+        offset += limit
 
